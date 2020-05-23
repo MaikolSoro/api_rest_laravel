164ef516dedee2f6fd3095d8a716ceaa7e15c7a7
Maikol Soro <maikolsoro.z1998@gmail.com>
2020-05-22 21:06:22 -0600

    Actualizar categoria

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -83,4 +83,45 @@ class CategoryController extends Controller
 
         return response()->json($data, $data['code']);
     }
+
+    /**
+    *
+    * Actualizar la categoría
+    *
+    */
+    public function update($id, Request $request) {
+
+        // Recoger los datos por post
+        $json = $request-> input('json', null);
+        $params_array = json_decode($json, true);
+
+        if(!empty($params_array)) {
+
+            // validar los datos
+            $validate = \Validate::make($params_array,[
+                'name' => 'required'
+            ]);
+
+            //Quitar lo que no quiero actualizar
+                unset($params_array['id']);
+                unset($params_array['created_at']);
+            //Actualizar el registro(categoria)
+            $category = Category:: where('id', $id) ->update($params_array);
+            $data = [
+                'code'=> 200,
+                'status' => 'success',
+                'category' => $params_array
+            ];
+        } else {
+            $data = [
+                'code' => 400,
+                'status' => 'error',
+                'message' => 'No has enviado ninguna categoria.'
+
+            ];
+        }
+         //Devolver respuesta
+        return response() ->json($data, $data['code']);
+
+    }
 }
